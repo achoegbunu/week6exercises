11b24c65261efacf0d4d837c452c41b34b763e44
acho <achoegbunu@gmail.com>
2018-07-01 16:58:30 -0700

    answerered the first 6 questions of week 6 exercises

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var testOne = "What kind of variable am I?";
+// testone is an object
+
+var testTwo = 256;
+// testTwo is a number with a value of 256
+
+var testThree = false;
+// testThree is a boolean with a value of 0
+
+var testFour = 'true';
+// testFour is a string with a single quote
+
+var testFive = '128';
+// testFive is a string with a value of 128
+
+var testSix = "";
+// testSix is a string with a value of ""
+
+
+var testOne = "hello" + "there";
+// testOne is a string with a value of "hellothere"
+
+var testTwo = "100" + 28;
+// testTwo is a string with a value of 10028
+
+var testThree = true + 3;
+// testThree is a boolean and a number with a value of 4
+
+var testFour = 'false' + 5;
