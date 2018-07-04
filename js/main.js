ef3eb8f8a176b851070781193dbda985ae4ca7ed
acho <achoegbunu@gmail.com>
2018-07-03 21:56:53 -0700

    answered question 6 on week 6 exercises

--- js/main.js
+++ js/main.js
@@ -24,6 +24,20 @@ var testTwo = "100" + 28;
 // testTwo is a string with a value of 10028
 
 var testThree = true + 3;
-// testThree is a boolean and a number with a value of 4
+// testThree is a boolean and a number which adds up to 4
 
 var testFour = 'false' + 5;
+// testFour is a single quote string and a number which adds up to "false5"
+
+var testFive = '128' - 3;
+// testFive is a string with a value of 125
+
+var testSix = "" * 9;
+// testSix consists of a string with a value of "" and a number with a value of 9 which equates to 0
+
+var testSeven = "zero" - 1;
+// testSeven consists of a string with no value and number with a value of 1 which equates to NaN (Not-a-Number)
+
+var testEight = 'five'.length;
+// testEight consists of a string with a value of "five" and a length property which equates to 4
+
