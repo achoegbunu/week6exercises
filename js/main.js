aae24030f0fa3e7788cbc4a26e4e43d8f06dc848
acho <achoegbunu@gmail.com>
2018-07-07 00:51:49 -0700

    finished week6 in class exercises

--- js/main.js
+++ js/main.js
@@ -83,3 +83,112 @@ var combinedNumbers = num1 + num2 + num3; // 120
 
 console.log(combinedNumbers); // 120
 
+var mixedNum1 = 10; // 10
+
+var mixedString1 = "ten"; // "ten"
+
+var mixedNum2 = "10"; // "10"
+
+var mixedConcatenation = mixedNum1 + mixedNum2 + mixedString1; // "1010ten"
+
+console.log(mixedConcatenation); // 1010ten
+
+var num4 = 5; // 5
+
+var num5 = 0; // 0
+
+if(num4 > 1) {
+	console.log("Num4 is greater than 1");
+} // Num4 is greater than 1
+
+else {
+	console.log("Num4 is NOT greater than 1");
+} // Num4 is not greater than 1
+
+var num4 = 5; // 5
+
+var num5 = 0; // 0
+
+if(num4 < num5 || num4 === num5) {
+	console.log("If statement ran!");
+} else if (num5 === "0" || true) {
+	console.log("Else if statement ran");
+} else {
+	console.log("Else statement ran");
+}; // Else if statement ran
+
+var num4 = 5;
+var num5 = 0;
+if(num4 > num5 && num4 === num5) {
+	console.log("If statement ran!");
+} else if (num5 === "0" || true) {
+	console.log("Else if statement ran");
+} else {
+	console.log("Else statement ran!");
+}; // else if statement ran
+
+var string1 = "this is a string";
+for(var i = 0; i < string1.length; i = i + 1) {
+	console.log(i, string1[i]);
+}; // "t" "h" "i" "s" " " "i" "s" " " "a" " " "s" "t" "r" "i" "n" "g"
+
+var string2 = "coding";
+var total = 0;
+for(var i = 0; i < string2.length; i++) {
+	total = total + i;
+};
+console.log(total);
+
+var string3 = "onomatopoeia";
+for(var i = 0; i < string3.length; i++) {
+	if(string3[i] == "o") {
+		console.log(i, string3[i]);
+	};
+};
+
+var string4 = "You're gonna need a bigger boat.";
+for(var i = 0; i < string4.length; i++) {
+	if(string4[i] == "a" || string4[i] == "e" || string4[i] == "i" || string4[i] == "o" || string4[i] == "u") {
+		console.log(string4[i]);
+	};
+};
+
+function testStuff(a,b) {
+	if(a > 25 && b < 100) {
+		console.log(a + " is greater than 25 and " + b + " is less than 100");
+	} else {
+		console.log(a + " is NOT greater than 25 or " + b + " is NOT less than 100");
+	};
+};
+testStuff(10, 101);
+testStuff(26, 99);
+testStuff(100, 25);
+
+function testStuff(c, d) {
+	if(c == 25) {
+		return c + " is equal to 25";
+	} else if (d <= 100) {
+		return d + " is less than or equal to 100";
+	} else {
+		return "Neither if nor else if statement ran";
+	};
+};
+testStuff(26, 101);
+testStuff(25, 99);
+testStuff(102, 24);
+
+function countVowels(someString) {
+	var vowels = 0;
+	for(var i = 0; i < someString.length; i++) {
+		if(someString[i] == "a" || someString[i] == "e" || someString[i] == "i" || someString[i] == "o" || someString[i] == "u") {
+			
+			vowels ++:
+		};
+	};
+
+	return vowels;
+};
+countVowels("I am the very model of a modern major general");
+countVowels("Exeunt pursued by a bear");
+countVowels("o1234567891o");
+
